0f655c0d562546708ab3cbef2ae9c87f199ab51f
Braden <braden.douglass@gmail.com>
2012-12-04 00:33:03 -0500

    added a simple utility to make creation of virtualhost a breeze

--- manifests/dev-tools.sh
+++ manifests/dev-tools.sh
@@ -10,4 +10,9 @@ apt-get -q -y install git-core
 
 apt-get -q -y install vim
 
+# Git Repo Needs
+
+# Handy util for on the fly vhost creation (because it is menotonous)
+git clone https://github.com/domichal/VirtualHostTool.git
+
 exit 0;
