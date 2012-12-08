f5ab986010f5c9feb1520427679716db0902f515
Braden <braden.douglass@gmail.com>
2012-12-08 16:09:33 -0500

    we need phar...accidentally deleted it, along with some updates to php.ini. Composer should work way better now

--- manifests/php5-tools.sh
+++ manifests/php5-tools.sh
@@ -5,4 +5,13 @@ cd /usr/bin
 wget -q http://getcomposer.org/composer.phar
 chmod a+x composer.phar
 
+# install phar
+cd /usr/bin
+wget -q http://pear2.php.net/pyrus.phar
+chmod 755 /usr/bin/pyrus.phar
+
+
+echo ';used to enable phar ext' >> /etc/php5/cli/php.ini
+echo 'suhosin.executor.include.whitelist="phar"' >> /etc/php5/cli/php.ini
+
 exit 0;
