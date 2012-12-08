9ff916a3050c1b141dab847e995c92bd3e7fcee6
Braden <braden.douglass@gmail.com>
2012-12-07 22:28:02 -0500

    cleaned up php-pear downloads. Composer should be able to do the heavy lifting and it is much easier on startup provisioning

--- manifests/php5-tools.sh
+++ manifests/php5-tools.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Upgrades PEAR.
-pear upgrade PEAR
-
-# Phing.
-pear channel-discover pear.phing.info
-pear install phing/phing
-
 # Composer.
 cd /usr/bin
 wget -q http://getcomposer.org/composer.phar
